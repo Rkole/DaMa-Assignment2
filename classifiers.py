526a42c9b5d370274e45f8051617dd0c58c0b190
rubenkole <46398896+rubenkole@users.noreply.github.com>
2022-10-21 12:35:37 +0200

    Finished normalization
    Next step is the creation of a vocabulary of the tokens in the documents

--- classifiers.py
+++ classifiers.py
@@ -5,16 +5,9 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
 
 
-
-
-
 def multinomial(X_train, X_test, y_train, y_test):
-
     clf = MultinomialNB()
     clf.fit(X_train, y_train)
     pred = clf.predict(X_test)
     matrix = confusion_matrix((pred, y_test))
     return matrix
-
-
-print(multinomial(train_test()[0], train_test()[1], train_test()[2], train_test()[3]))
--- dataset.py
+++ dataset.py
@@ -6,11 +6,19 @@ from sklearn.model_selection import train_test_split
 from nltk.stem import WordNetLemmatizer
 import re
 from nltk.corpus import stopwords
+from collections import Counter
 
 
+# Run one time:
 # nltk.download('omw-1.4')
+
+
 def make_dataset():
-    # If truthful == 1, it is a truthful review, else its deceptive
+    """
+    Make a dataframe containing all sentences and classifications.
+    :return: Dataframe
+    """
+    # If truthful == 1, it is a truthful review, else it is deceptive
     data_list = []
 
     for folder in os.listdir("data/deceptive"):
@@ -23,11 +31,16 @@ def make_dataset():
             with open("data/truthful/{}/{}".format(folder, file)) as f:
                 review = f.read()
                 data_list.append({"sentence": review, 'truthful': 1})
+
     dataframe = pd.DataFrame.from_records(data_list)
     return dataframe
 
 
 def train_test():
+    """
+    Split the dataset into training and testing sets
+    :return:
+    """
     train, test = train_test_split(make_dataset(), random_state=8, train_size=0.8, test_size=0.2)
     X_train = np.array(train['sentence'])
     X_test = np.array(test['sentence'])
@@ -37,23 +50,48 @@ def train_test():
     return X_train, X_test, y_train, y_test
 
 
-df = train_test()
+datas = train_test()
 
 
 def normalize(sentences):
+    """
+
+    :param sentences: dataframe with sentences
+    :return: dataframe with updated sentences
+    """
     stop_words = set(stopwords.words('english'))
     wnl = WordNetLemmatizer()
     counter = 0
+    new_list = []
     for sentence in sentences:
-        sentence = sentence.lower()
+        sentence = sentence.lower()  # Make the sentence lowercase
         sentence = re.sub(r'\S+.com', '', sentence)  # Remove sites and mail addresses
         sentence = re.sub(r'[^\w\s]', '', sentence)  # Remove interpunction
-        sentence = nltk.word_tokenize(sentence)
-        sentence = ' '.join([wnl.lemmatize(str(words)) for words in sentence])
-        sentence = [word for word in sentence if not word in stopwords.words()]
+        sentence = re.sub(r'[0-9]', " ", sentence)  # Remove numeric values
+        tokens = nltk.word_tokenize(sentence)  # Tokenize the words
+        tokens = [word for word in tokens if
+                  not word in stopwords.words()]  # Remove stop words (Slows down the program significantly)
+        tokens = ' '.join([wnl.lemmatize(str(words)) for words in tokens])  # Lemmatize words
+        tokens = nltk.word_tokenize(tokens)
+        new_dict = {}
+        new_dict.update({"sentence": tokens})
+        new_list.append(new_dict)
+
         counter += 1
-
         print("Done:", (counter / len(sentences)) * 100, "%")
+    new_df = pd.DataFrame(new_list)
+    return new_df
 
 
-normalize(df[0])
+X_train_norm = normalize(datas[0][0:2])
+print(X_train_norm)
+
+# def make_vocab(document, vocab):
+#    for i in document:
+
+
+#vocabulary = Counter()
+
+# make_vocab(X_train_norm, vocabulary)
+
+#print(vocabulary.most_common(50))
