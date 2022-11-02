1f16ff335d4b078fd8ee3b359056630d4e39b784
rubenkole <ruben12kole@gmail.com>
2022-11-02 10:18:15 +0100

    training for parameter max_param

--- classifiers.py
+++ classifiers.py
@@ -1,6 +1,5 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from dataset import train_test
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.metrics import confusion_matrix
 from sklearn.naive_bayes import MultinomialNB
--- dataset.py
+++ dataset.py
@@ -1,24 +1,21 @@
-import nltk
 import pandas as pd
-import numpy as np
 import os
-from sklearn.model_selection import train_test_split
 from nltk.stem import WordNetLemmatizer
 import re
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
 from nltk.corpus import stopwords
-from collections import Counter
-
-
+from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
+from sklearn.model_selection import cross_val_score, RandomizedSearchCV
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import LogisticRegressionCV
 # Run one time:
 # nltk.download('omw-1.4')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
 
 
 def make_dataset():
-    """
-    Make a dataframe containing all sentences and classifications.
-    :return: Dataframe
-    """
-    # If truthful == 1, it is a truthful review, else it is deceptive
     data_list = []
 
     for folder in os.listdir("data/deceptive"):
@@ -27,6 +24,7 @@ def make_dataset():
                 review = f.read()
                 data_list.append({"sentence": review, 'truthful': 0})
 
+    for folder in os.listdir("data/truthful"):
         for file in os.listdir("data/truthful/{}".format(folder)):
             with open("data/truthful/{}/{}".format(folder, file)) as f:
                 review = f.read()
@@ -36,62 +34,80 @@ def make_dataset():
     return dataframe
 
 
-def train_test():
-    """
-    Split the dataset into training and testing sets
-    :return:
-    """
-    train, test = train_test_split(make_dataset(), random_state=8, train_size=0.8, test_size=0.2)
-    X_train = np.array(train['sentence'])
-    X_test = np.array(test['sentence'])
-    y_train = np.array(train['truthful'])
-    y_test = np.array(test['truthful'])
-
-    return X_train, X_test, y_train, y_test
-
-
-datas = train_test()
-
-
 def normalize(sentences):
-    """
-
-    :param sentences: dataframe with sentences
-    :return: dataframe with updated sentences
-    """
-    stop_words = set(stopwords.words('english'))
     wnl = WordNetLemmatizer()
-    counter = 0
-    new_list = []
+    documents = []
+
     for sentence in sentences:
         sentence = sentence.lower()  # Make the sentence lowercase
+
         sentence = re.sub(r'\S+.com', '', sentence)  # Remove sites and mail addresses
         sentence = re.sub(r'[^\w\s]', '', sentence)  # Remove interpunction
         sentence = re.sub(r'[0-9]', " ", sentence)  # Remove numeric values
-        tokens = nltk.word_tokenize(sentence)  # Tokenize the words
-        tokens = [word for word in tokens if
-                  not word in stopwords.words()]  # Remove stop words (Slows down the program significantly)
-        tokens = ' '.join([wnl.lemmatize(str(words)) for words in tokens])  # Lemmatize words
-        tokens = nltk.word_tokenize(tokens)
-        new_dict = {}
-        new_dict.update({"sentence": tokens})
-        new_list.append(new_dict)
 
-        counter += 1
-        print("Done:", (counter / len(sentences)) * 100, "%")
-    new_df = pd.DataFrame(new_list)
-    return new_df
+        document = sentence.split()
+        document = [wnl.lemmatize(word) for word in document]
+        document = ' '.join(document)
+
+        documents.append(document)
+    return documents
 
 
-X_train_norm = normalize(datas[0][0:2])
-print(X_train_norm)
+all_data = make_dataset()
+X = all_data.iloc[:, 0]
+y = all_data.iloc[:, 1]
+X_norm = normalize(X)
 
-# def make_vocab(document, vocab):
-#    for i in document:
+for i in [5000, 10000, 15000, 25000, 50000, 75000]:
+    vectorizer = CountVectorizer(max_features=i, stop_words=stopwords.words('english'), ngram_range=(2, 2))
+    X = vectorizer.fit_transform(X_norm).toarray()
+    X = TfidfTransformer().fit_transform(X).toarray()
+
+    penalty_logreg = ['l1', 'l2']
+
+    alpha_list = [0.1, 1, 10]
+
+    highest_score = 0
+    for i in penalty_logreg:
+        clf = LogisticRegressionCV(penalty=i, solver='liblinear', max_iter=100000)
+        result = cross_val_score(clf, X, y, cv=10, scoring='accuracy').mean()
+        if result > highest_score:
+            highest_score = result
+            best_parameter = i
+
+    print(highest_score, best_parameter)
+    highest_score = 0
+
+    for alpha in alpha_list:
+        clf = MultinomialNB(alpha=alpha)
+        result = cross_val_score(clf, X, y, cv=10, scoring='accuracy').mean()
+        if result > highest_score:
+            highest_score = result
+            best_parameter = alpha
+
+    print(highest_score, best_parameter)
+
+    tree1 = DecisionTreeClassifier()
+    distribution_tree = dict(criterion=['gini', 'entropy'],
+                             max_depth=[x for x in range(10, 100, 10)],
+                             min_samples_split=[x for x in range(2, 10)],
+                             min_samples_leaf=[x for x in range(1, 5)])
+
+    forest1 = RandomForestClassifier()
+    distribution_forest = dict(criterion=['gini', 'entropy'],
+                               max_depth=[x for x in range(10, 100, 10)],
+                               min_samples_split=[x for x in range(2, 10)],
+                               min_samples_leaf=[x for x in range(1, 5)],
+                               max_features=['sqrt', 'log2'],
+                               n_estimators=[x for x in range(50, 150, 10)])
 
 
-#vocabulary = Counter()
+    def best_param(clf, dist, n_iter=35):
+        clf = RandomizedSearchCV(clf, dist, n_iter=n_iter, cv=10)
+        search = clf.fit(X, y)
+        print(search.best_params_)
 
-# make_vocab(X_train_norm, vocabulary)
 
-#print(vocabulary.most_common(50))
+    best_param(tree1, distribution_tree)
+    best_param(forest1, distribution_forest)
+
